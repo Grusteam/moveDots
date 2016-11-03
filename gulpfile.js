7fc0b3896f669c6c0238ed726af867aaac51e172
Александр <popovichenko14111993@gmail.com>
2016-11-03 16:22:46 +0300

    New version with readme.md

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,5 @@
 'use strict';
-
+// Поключение зависимостей
 var gulp = require('gulp'),
     cleanCSS = require('gulp-clean-css'),
     rigger = require('gulp-rigger'),
@@ -18,11 +18,11 @@ var path = {
         styles: 'build/styles/',
         content: 'build/content/',
         images: 'build/images/',
-        fonts: 'build/fonts/'
+        fonts: 'build/fonts/',
+        modules: 'build/node_modules'
     },
     src: { //Пути откуда брать исходники
         html: 'src/*.html', //Синтаксис src/*.html говорит gulp что мы хотим взять все файлы с расширением .html
-        modules: 'node_modules/*.*', // подключенные модули
         js: 'src/js/*.js',//В стилях и скриптах нам понадобятся только main файлы
         data: 'src/data/*.json', //Папка для тестовых данных в формате json
         styles: 'src/styles/*.css',
@@ -41,17 +41,17 @@ var path = {
     },
     clean: './build'
 };
-
+// Сборка html и фавикон
 gulp.task('html:build', function () {
     gulp.src(path.src.html) //Выберем файлы по нужному пути
         .pipe(rigger())
         .pipe(gulp.dest(path.build.html)) //Выплюнем их в папку build
-        .pipe(browserSync.reload({stream:true})); //И перезагрузим наш сервер для обновлений
+        .pipe(browserSync.stream());
         
     gulp.src(path.src.favicon)
         .pipe(gulp.dest('build/'));
 });
-
+// Сборка стилей
 gulp.task('style:build', function () {
     gulp.src(path.src.styles)
         .pipe(sourcemaps.init())
@@ -59,57 +59,65 @@ gulp.task('style:build', function () {
         .pipe(cleanCSS())
         .pipe(sourcemaps.write('.'))
         .pipe(gulp.dest(path.build.styles))
-        .pipe(browserSync.reload({stream:true}));
+        .pipe(browserSync.stream());
 });
-
+// Сборка шрифтов
 gulp.task('fonts:build', function() {
     gulp.src(path.src.fonts)
         .pipe(gulp.dest(path.build.fonts))
+        .pipe(browserSync.stream());
 });
-
+// Сборка и сжатие изображений
 gulp.task('image:build', function () {
     gulp.src(path.src.images) //Выберем наши картинки
         .pipe(gulp.dest(path.build.images)) //И бросим в build
-        .pipe(browserSync.reload({stream:true}));
+        .pipe(browserSync.stream());
 
     gulp.src(path.src.content) //Выберем наши картинки
         .pipe(gulp.dest(path.build.content)) //И бросим в build
-        .pipe(browserSync.reload({stream:true}));
+        .pipe(browserSync.stream());
 });
-
+// Сборка js и json
 gulp.task('js:build', function () {
     gulp.src(path.src.data) //Найдем наш main файл
-        .pipe(gulp.dest(path.build.data));
+        .pipe(gulp.dest(path.build.data))
+        .pipe(browserSync.stream());
 
     gulp.src(path.src.js) //Найдем наш main файл
         .pipe(gulp.dest(path.build.js)) //Выплюнем готовый файл в build
-        .pipe(browserSync.reload({stream:true})); //И перезагрузим сервер
+        .pipe(browserSync.stream());
 });
+// Перенос зависимостей в build
+gulp.task('clean-module', function() {
+    return gulp.src(path.build.modules)
+        .pipe(clean(path.build.modules));
+});
+gulp.task('module', ['clean-module'], function() {
+    gulp.src(gnf(), {base:'./'})
+        .pipe(gulp.dest('./build'));
+});
+gulp.task('re-module', ['module'], function() {
+    gulp.src('./package.json')
+        .pipe(browserSync.stream());
+});
+
 // Построение структуры Build
 gulp.task('build', [
     'html:build',
     'js:build',
     'style:build',
     'fonts:build',
-    'image:build'
+    'image:build',
+    'module'
 ]);
 // Отслеживание изменений файлов
 gulp.task('watch', function(){
-    gulp.watch([path.watch.html], function(event, cb) {
-        gulp.start('html:build');
-    });
-    gulp.watch([path.watch.styles], function(event, cb) {
-        gulp.start('style:build');
-    });
-    gulp.watch([path.watch.js, path.watch.data], function(event, cb) {
-        gulp.start('js:build');
-    });
-    gulp.watch([path.watch.images], function(event, cb) {
-        gulp.start('image:build');
-    });
-    gulp.watch([path.watch.fonts], function(event, cb) {
-        gulp.start('fonts:build');
-    });
+    gulp.watch([path.watch.html], ['html:build']);
+    gulp.watch([path.watch.styles], ['style:build']);
+    gulp.watch([path.watch.js, path.watch.data], ['js:build']);
+    gulp.watch([path.watch.images], ['image:build']);
+    gulp.watch([path.watch.fonts], ['fonts:build']);
+    gulp.watch('./package.json', ['re-module']);
 });
 // Очистка папки build
 gulp.task('clean', function () {
@@ -121,8 +129,7 @@ gulp.task('server', function() {
         server: {
             baseDir: "./build"
         },
-        port: 8080, 
-        tunnel: true
+        port: 8080
     });
 });
 // Сжатие изображений
@@ -132,4 +139,7 @@ gulp.task('image', function() {
         .pipe(gulp.dest(path.build.images))
 });
 // Команда Gulp
-gulp.task('default', ['build', 'server', 'watch']);
+gulp.task('default', ['build'], function() {
+    gulp.start('server');
+    gulp.start('watch');
+});
