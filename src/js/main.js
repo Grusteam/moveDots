4a3df2219dcbec6d34c68ef9278fee4b57dd334f
Dergachev Mihail <dergachev.mihail@gmail.com>
2016-11-11 13:16:34 +0300

    Update routes

--- src/js/main.js
+++ src/js/main.js
@@ -1,25 +1,41 @@
 /* eslint-disable */
 
+var router; // global
+
+var App = {
+	init: function() {
+		var routes = {
+			'/': function() {
+				console.log('index');
+			},
+			
+			'/feedback': function() {
+				console.log('feedback');
+			},
+			
+			'/feedback/:user': function(user) {
+				console.log('feedback/user', user);
+			},
+			
+			'/search': {
+				'\/(.*)': {
+					on: function(request) {
+						var params = decodeURI(request).split('&');
+						console.log(':request:', params);
+					}
+				}
+			}
+		};
+		
+		router = Router(routes).configure({
+			html5history: true
+		}).init();
+	}
+}
+
+
 $(function() {
-	var routes = {
-		'/': function() {
-			console.log('index');
-		},
-		'/feedback': function() {
-			console.log('feedback');
-		},
-		'/feedback/user': function() {
-			console.log('/feedback/user');
-		},
-		'/route/:type': function(type) {
-			console.log('param:', type);
-		}
-	};
-	
-	var router = Router(routes).configure({
-		html5history: true
-	}).init();
-	
+	App.init();
 	
 	$('.js-route').on('click', function(event) {
 		event.preventDefault();
